a4b9613ff8f5a21ad22e16e6b8c51c39825ca19a
Norsang Nyandak <TNorsang>
2022-11-06 01:54:00 -0400

    aded functions

--- js/map.js
+++ js/map.js
@@ -1,25 +1,45 @@
 
+var ic = "default.png";
 // Initialize and add the map
 function initMap(x = 0,y= 0){
   lats = x;
   longs = y;
+ 
   window.initMap = campMap;
-  
+
+}
+function initMap2(x = 0,y= 0, zoom = 14, imgOFIcon){
+  lats = x;
+  longs = y;
+  zum = zoom;
+  ic = imgOFIcon;
+
+  window.initMap = campMap;
+
 }
 
-function campMap(i=40.7127281,j=-74.0060152) {
-
+function campMap(i=40.7127281,j=-74.0060152, zum = 14) {
+    
     var lats=i;
     var longs=j;
     const area = { lat: lats, lng: longs};
-    
+    const icon = {
+      url: `../Icons/${ic}`, // url
+      scaledSize: new google.maps.Size(50, 50), // scaled size
+      origin: new google.maps.Point(0,0), // origin
+      anchor: new google.maps.Point(0, 0) // anchor
+  };
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 14,
+      zoom: zum,
       center: area,
     });
     // Marker
-    const marker = new google.maps.Marker({
+
+ 
+    
+    var marker = new google.maps.Marker({
       position: area,
+      icon: icon,
       map: map,
     });
   }
---
+++ js/map2.js
@@ -0,0 +1,84 @@
+
+var ic = "default.png";
+
+class Person {
+  name;
+  geoX;
+  geoY;
+  icon;
+  
+  constructor(name) {
+    this.name = name;
+    this.geoX = 0;
+    this.geoY = 0;
+    this.icon = "default";
+  }
+
+  setGeoXANDY(geoX,geoY){
+    this.geoX = geoX;
+    this.geoY = geoY;
+  }
+
+  setIcon(ic){
+    this.icon = ic;
+  }
+
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name} I am at location ${this.geoX} , ${this.geoY}`);
+  }
+
+}
+
+
+function initMap(x = 0,y= 0,ic = "img"){
+  lats = x;
+  longs = y;
+  console.log("long is" + longs);
+ 
+  console.log("lat is" + lats);
+  
+  
+  window.initMap = campMap;
+  
+
+}
+
+function campMap(i=0.7127281,j=-74.0060152, ic = "girl1") {
+  
+
+    var lats=i;
+    var longs=j;
+    var area = { lat: lats, lng: longs};
+    
+ 
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 14,
+      center: area,
+    });
+    // Marker    
+    // var marker = new google.maps.Marker({
+    //   position: area,
+    //   icon:`../Icons/${ic}.png` ,
+    //   map: map,
+    // });
+    createMarker({ lat: lats+0.03, lng: longs-0.02}, "girl2" , map);
+    createMarker(area, ic , map);
+    createMarker({ lat: lats+0.1, lng: longs}, "guy2" , map);
+  }
+
+  function createMarker(area,ic, map){
+    new google.maps.Marker({
+      position: area,
+      icon:`../Icons/${ic}.png` ,
+      map: map,
+    });
+  }
+ 
+  initMap();
+  // document.getElementById("hither").addEventListener("click",alert("hi"));
+
+  // window.initMap is the set syntax to set a map
+
+
+  
+  
